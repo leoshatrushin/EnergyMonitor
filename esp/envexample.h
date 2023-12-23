3865ca1dddc6815c0ac04b4be671e591d83c675b
leoshatrushin <leoshatrushin@gmail.com>
2023-12-23 20:37:51 +0800

    update esp env config

---
+++ esp/envexample.h
@@ -0,0 +1,7 @@
+// create a file caled env.h and copy or #include these macros
+#define WIFI_SSID "ssid"
+#define WIFI_PASS "password"
+#define WIFI_MIN_AUTHMODE WIFI_AUTH_WPA2_PSK
+#define SERVER_HOSTNAME "example.com"
+#define SERVER_PORT 3000
+#define API_KEY "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
