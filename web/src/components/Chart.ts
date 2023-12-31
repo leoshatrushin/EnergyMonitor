248ce3be641e5e2a3bd103032001ca63f7465908
leoshatrushin <leoshatrushin@gmail.com>
2023-12-31 17:13:31 +0800

    refactor frontend

--- web/src/components/Chart.ts
+++ web/src/components/Chart.ts
@@ -1,14 +1,12 @@
-import { getData, getStream } from '../services/API';
+import { getData, installTimestampHandler } from '../services/API';
 import { select } from 'd3-selection';
 import { scaleBand, scaleLinear, scaleTime } from 'd3-scale';
 import { max } from 'd3-array';
 import { axisBottom, axisLeft } from 'd3-axis';
+import { roundDown } from '../../../common/utils';
+import { MINUTE, TIMESTAMP_SIZE, FILE_OFFSET_SIZE } from '../common/constants';
 
 type Bar = { time: number; value: number };
-
-function roundDownToMinute(timestamp: number) {
-    return timestamp - (timestamp % (60 * 1000));
-}
 
 function numBars(start: number, end: number, interval: number) {
     return Math.floor((end - start) / interval) + 1;
@@ -17,64 +15,77 @@ function numBars(start: number, end: number, interval: number) {
 function convertBinToBars(bin: ArrayBuffer, start: number, end: number): Bar[] {
     const dataView = new DataView(bin);
     let barData = [];
-    let prev = dataView.getUint32(0, true) / 8;
-    for (let i = 4; i < bin.byteLength; i += 4) {
-        const time = start + ((i - 1) / 4) * 60 * 1000;
-        const rawValue = dataView.getUint32(i, true) / 8;
+    let prev: number;
+    if (bin.byteLength >= 4) {
+        prev = dataView.getUint32(0, true) / TIMESTAMP_SIZE;
+    }
+    for (let i = FILE_OFFSET_SIZE; i < bin.byteLength; i += FILE_OFFSET_SIZE) {
+        const time = start + ((i - 1) / FILE_OFFSET_SIZE) * MINUTE;
+        const rawValue = dataView.getUint32(i, true) / TIMESTAMP_SIZE;
         const value = rawValue - prev;
         prev = rawValue;
         barData.push({ time, value });
     }
-    const zeroBarsToAdd = numBars(start, end, 60 * 1000) - barData.length;
+    const zeroBarsToAdd = numBars(start, end, MINUTE) - barData.length;
     for (let i = 0; i < zeroBarsToAdd; i++) {
-        barData.push({ time: start + (barData.length + i) * 60 * 1000, value: 0.1 });
+        barData.push({ time: start + (barData.length + i) * MINUTE, value: 0.1 });
     }
     return barData;
 }
 
-const now = roundDownToMinute(Date.now());
-let start = now - 14 * 60 * 1000;
+const svg = select('#chartData');
+const svgWidth = (svg.node() as HTMLElement).getBoundingClientRect().width;
+const svgHeight = (svg.node() as HTMLElement).getBoundingClientRect().height;
+const barsMargin = { top: 0, right: 0, bottom: 40, left: 20 };
+const barsWidth = svgWidth - barsMargin.left - barsMargin.right;
+const barsHeight = svgHeight - barsMargin.top - barsMargin.bottom;
+
+const now = roundDown(Date.now(), MINUTE);
+let start = now - 14 * MINUTE;
 let end = now;
+
 const minutesBin = await getData(start, end);
 const bars = convertBinToBars(minutesBin, start, end);
-const svgElement = document.getElementById('chartData');
-let svWidth = svgElement.getAttribute('width');
-let svgHeight = svgElement.getAttribute('height');
-
-const svg = select('#chartData');
 
 function updateData(data: Bar[]) {
-    const xTime = scaleTime().range([40, 440]);
-    const xBand = scaleBand().range([40, 440]).padding(0.1);
-    const yScale = scaleLinear().range([420, 20]);
-    xTime.domain([new Date(start), new Date(end + 60 * 1000)]);
-    xBand.domain(bars.map((d: Bar) => String(d.time)));
-    yScale.domain([0, max(bars, (d: Bar) => d.value) ?? 0]);
+    const xTime = scaleTime()
+        .domain([new Date(start), new Date(end + MINUTE)])
+        .range([0, barsWidth]);
+    const xBand = scaleBand()
+        .domain(bars.map((d: Bar) => String(d.time)))
+        .range([0, barsWidth])
+        .padding(0.25);
+    const yScale = scaleLinear()
+        .domain([0, max(bars, (d: Bar) => d.value)])
+        .range([barsHeight, 0]);
+
     svg.selectAll('rect')
         .data(data)
         .join(
             function (enter) {
                 return enter
                     .append('rect')
-                    .attr('x', (d: Bar) => xBand(String(d.time)) ?? 0)
-                    .attr('y', (d: Bar) => yScale(d.value))
-                    .attr('height', (d: Bar) => 420 - yScale(d.value))
+                    .attr('x', (d: Bar) => barsMargin.left + xBand(String(d.time)))
+                    .attr('y', (d: Bar) => barsMargin.top + yScale(d.value))
+                    .attr('height', (d: Bar) => barsHeight - yScale(d.value))
                     .attr('width', xBand.bandwidth())
                     .attr('fill', 'cyan')
-                    .attr('opacity', '0.8')
+                    .attr('opacity', '0.9')
                     .attr('class', 'glow');
             },
             function (update) {
                 return update
-                    .attr('x', (d: Bar) => xBand(String(d.time)) ?? 0)
-                    .attr('y', (d: Bar) => yScale(d.value))
-                    .attr('height', (d: Bar) => 420 - yScale(d.value));
+                    .attr('x', (d: Bar) => barsMargin.left + xBand(String(d.time)))
+                    .attr('y', (d: Bar) => barsMargin.top + yScale(d.value))
+                    .attr('height', (d: Bar) => barsHeight - yScale(d.value));
             },
             function (exit) {
                 return exit.remove();
             },
         );
-    svg.select('.x-axis').remove();
+
+    // add x-axis
+    svg.select('.x-axis').selectAll('*').remove();
     const xAxis = axisBottom(xTime)
         .tickFormat((date: Date) => {
             // Format hours and minutes with leading zeros if needed
@@ -84,38 +95,47 @@ function updateData(data: Bar[]) {
         })
         .ticks(5)
         .tickSizeOuter(0);
-    svg.append('g').attr('class', 'x-axis').attr('transform', `translate(0,422)`).call(xAxis);
-    svg.select('.y-axis').remove();
+    const axisBottomPaddingTop = 2;
+    svg.select('g.axisBottom')
+        // @ts-ignore
+        .call(xAxis)
+        .attr('transform', `translate(${barsMargin.left}, ${barsHeight + barsMargin.top + axisBottomPaddingTop})`);
+    // .selectAll('text')
+    // .attr('transform', 'translate(-10,10)rotate(-45)');
+
+    // add y-axis
+    svg.select('.axisLeft').selectAll('*').remove();
     const yAxis = axisLeft(yScale)
         .ticks(5)
         .tickFormat(d => {
-            return `${Number(d)}KW`;
+            return `${Number(d)}`;
         })
-        .tickSizeOuter(0);
-    svg.append('g').attr('class', 'y-axis').attr('transform', `translate(40,2)`).call(yAxis);
+        .tickSizeOuter(0)
+        .tickSizeInner(-svgWidth);
+    svg.select('g.axisLeft')
+        // @ts-ignore
+        .call(yAxis)
+        .attr('transform', `translate(${barsMargin.left}, ${barsMargin.top + axisBottomPaddingTop})`);
 }
 
 updateData(bars);
 
-const sse = await getStream(now);
-sse.onmessage = function incorporateTimestamp(e: MessageEvent) {
-    const timestamp = parseInt(e.data);
-    console.log(timestamp);
-    const timestampMinute = roundDownToMinute(timestamp);
-    if (timestamp - end < 60 * 1000) {
+installTimestampHandler(function incorporateTimestamp(timestamp: number) {
+    const timestampMinute = roundDown(timestamp, MINUTE);
+    if (timestamp - end < MINUTE) {
         console.log('adding');
-        bars[(timestampMinute - start) / 60 / 1000].value++;
+        bars[(timestampMinute - start) / MINUTE].value++;
     } else {
         console.log('new minute');
-        const minDiff = Math.floor((timestamp - end) / 60 / 1000);
+        const minDiff = Math.floor((timestamp - end) / MINUTE);
         for (let i = 0; i < minDiff - 1; i++) {
             bars.shift();
-            bars.push({ time: timestampMinute - (minDiff - i) * 60 * 1000, value: 0.3 });
+            bars.push({ time: timestampMinute - (minDiff - i) * MINUTE, value: 0.3 });
         }
         bars.shift();
         bars.push({ time: timestampMinute, value: 1 });
-        start = start + minDiff * 60 * 1000;
+        start = start + minDiff * MINUTE;
         end = timestampMinute;
     }
     updateData(bars);
-};
+});
--- web/src/services/API.ts
+++ web/src/services/API.ts
@@ -1,14 +1,74 @@
+import { TIMESTAMP_SIZE } from '../common/constants';
+
 const PROTOCOL = import.meta.env.VITE_PROTOCOL;
+const WS_PROTOCOL = PROTOCOL === 'https' ? 'wss' : 'ws';
 const HOST = import.meta.env.VITE_HOST;
 const PORT = import.meta.env.VITE_HOST_PORT;
 
+const ws = new WebSocket(`${WS_PROTOCOL}://${HOST}:${PORT}`);
+ws.binaryType = 'arraybuffer';
+await new Promise(resolve => (ws.onopen = resolve));
+
+let receivingData = false;
+let receivingDataResolver: (value: ArrayBuffer) => void;
+
 export async function getData(start: number, end: number) {
-    const res = await fetch(`${PROTOCOL}://${HOST}:${PORT}/data?s=${start}&e=${end}`);
-    const bin = await res.arrayBuffer();
-    return bin;
+    const buffer = new ArrayBuffer(TIMESTAMP_SIZE * 2);
+    const view = new DataView(buffer);
+    view.setBigUint64(0, BigInt(start), true);
+    view.setBigUint64(TIMESTAMP_SIZE, BigInt(end), true);
+    ws.send(buffer);
+
+    // wait for data to be received
+    const data = await new Promise<ArrayBuffer>(resolve => {
+        receivingDataResolver = resolve;
+    });
+    return data;
 }
 
-export async function getStream(start: number) {
-    const sse = new EventSource(`${PROTOCOL}://${HOST}:${PORT}/stream?s=${start}`);
-    return sse;
+const timeStampHandlers: ((timestamp: number) => void)[] = [];
+export function installTimestampHandler(handler: (timestamp: number) => void) {
+    timeStampHandlers.push(handler);
 }
+
+let dataBuffer: ArrayBuffer = new ArrayBuffer(0);
+let numDataBytes = 0;
+
+ws.onmessage = function (event) {
+    const data: ArrayBuffer = event.data;
+
+    if (receivingData) {
+        // concatenate data
+        const concatenatedBuffer = new ArrayBuffer(dataBuffer.byteLength + data.byteLength);
+        const concatenatedView = new Uint8Array(concatenatedBuffer);
+        concatenatedView.set(new Uint8Array(dataBuffer), 0);
+        concatenatedView.set(new Uint8Array(data), dataBuffer.byteLength);
+        dataBuffer = concatenatedBuffer;
+
+        // check if all data has been received
+        numDataBytes -= data.byteLength;
+        if (numDataBytes === 0) {
+            receivingData = false;
+            receivingDataResolver(dataBuffer);
+        }
+
+        return;
+    }
+
+    const view = new DataView(data);
+    const timestamp = Number(view.getBigUint64(0, true));
+
+    // check if message is a data header
+    if (timestamp === 0) {
+        numDataBytes = view.getUint32(8, true);
+        console.log(`Receiving ${numDataBytes} bytes of data`);
+        if (numDataBytes > 0) receivingData = true;
+        else receivingDataResolver(new ArrayBuffer(0));
+        return;
+    }
+
+    console.log(timestamp);
+    for (const handler of timeStampHandlers) {
+        handler(timestamp);
+    }
+};
